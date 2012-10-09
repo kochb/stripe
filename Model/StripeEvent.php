47f9e830084dc749937c0752318d6b41701ed01a
Brad Koch <brad@farmlogs.com>
2012-10-09 12:48:04 -0400

    StripeEvent model added

---
+++ Model/StripeEvent.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Stripe event
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2011, Jeremy Harris
+ * @link http://42pixels.com
+ * @package stripe
+ * @subpackage stripe.models
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('StripeAppModel', 'Stripe.Model');
+
+/**
+ * StripeEvent
+ *
+ * @package stripe.models
+ */
+class StripeEvent extends StripeAppModel {
+
+/**
+ * API path
+ *
+ * @var string
+ */
+	public $path = '/events';
+
+/**
+ * Event schema
+ *
+ * @var array
+ */
+	public $_schema = array(
+		'id' => array('type' => 'string'),
+		'number' => array('type' => 'string'),
+		'livemode' => array('type' => 'boolean'),
+		'created' => array('type' => 'string'),
+		'data' => array('type' => 'string'),
+		'pending_webhooks' => array('type' => 'integer'),
+		'type' => array('type' => 'string'),
+	);
+
+}
