b27b551e75ea65ae3e2ea799e3fb0720afd940ba
Brad Koch <brad@farmlogs.com>
2013-01-31 15:09:37 -0500

    Strict fixes

--- Model/Datasource/StripeSource.php
+++ Model/Datasource/StripeSource.php
@@ -98,9 +98,7 @@ class StripeSource extends DataSource {
  * @param array $queryData Query data (conditions, limit, etc)
  * @return mixed `false` on failure, data on success
  */
-	public function read(Model $model, $queryData = null, $recursive = null) {
-        if (empty($queryData)) $queryData = array();
-
+	public function read(Model $model, $queryData = array(), $recursive = null) {
 		// If calculate() wants to know if the record exists. Say yes.
 		if ($queryData['fields'] == 'COUNT') {
 			return array(array(array('count' => 1)));
